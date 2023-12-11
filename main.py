b13f6d97202e79e4dd86ec2360387676f0e88bb5
Adithya Rajendran <adithyaraj@gmail.com>
2023-12-10 22:57:08 -0800

    loaded the background board

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import pygame
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+WIDTH = HEIGHT = 512  # width and height of the chess board
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    original_background = pygame.image.load("Assets/board.png")
+    background = pygame.transform.scale(original_background, (WIDTH, HEIGHT))
+
+    run = True
+    while run:
+        screen.blit(background, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+    pygame.quit()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
